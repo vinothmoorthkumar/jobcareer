7deec053c93e3193db152327400f09f5c4bb7aea
vinoth <vinothmoorthy.kumar@gmail.com>
2022-08-08 16:53:27 +0400

    updated nav

--- client/admin/src/app/app.module.ts
+++ client/admin/src/app/app.module.ts
@@ -10,19 +10,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { AlertComponent } from './_components';
-import { HomeComponent } from './home';
+import { HomeComponent } from './home';;
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
+        CommonModule,
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
-        AppRoutingModule
-    ],
+        AppRoutingModule,
+        BrowserAnimationsModule ,
+        LayoutModule ,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule
+          ],
     declarations: [
         AppComponent,
         AlertComponent,
-        HomeComponent
+        HomeComponent,
+        NavComponent],
+    exports:[
+        CommonModule
     ],
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
