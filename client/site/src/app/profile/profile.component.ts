97bdaddbd8c2f8edb4ad16c3ce2af21f901dc032
vinoth <vinothmoorthy.kumar@gmail.com>
2022-08-10 19:40:12 +0400

    updated indust

--- client/site/src/app/profile/profile.component.ts
+++ client/site/src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 import { UsersService } from '@app/_services';
+import {NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
@@ -12,6 +13,10 @@ export class ProfileComponent implements OnInit {
   submitted = false;
   loading = false;
   profileData;
+  model: NgbDateStruct;
+  especialDates:NgbDateStruct[]=[
+    {year:2018,month:6,day:1},
+    {year:2018,month:6,day:10}]
 
   constructor(private formBuilder: FormBuilder, public usersService:UsersService) { }
 
@@ -19,13 +24,36 @@ export class ProfileComponent implements OnInit {
     this.getData();
 
     this.form = this.formBuilder.group({
-      // password: ['', Validators.required],
       mobile: ['', [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")]],
       email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
       name: ['', Validators.required],
+      workExp: new FormArray([])
+      
     });
 
     this.form.controls['email'].disable();
+  }
+
+  get workExp() : FormArray {
+    return this.form.get("workExp") as FormArray
+  }
+ 
+  newExp(): FormGroup {
+    return this.formBuilder.group({
+      companyName: '',
+      jobTitle: '',
+      startDate:'',
+      endDate:'',
+      skills:''
+    })
+  }
+ 
+  addExp() {
+    this.workExp.push(this.newExp());
+  }
+ 
+  removeExp(i:number) {
+    this.workExp.removeAt(i);
   }
 
   getData(){
@@ -36,6 +64,7 @@ export class ProfileComponent implements OnInit {
         mobile:this.profileData.mobile,
         email:this.profileData.email,
         name:this.profileData.name,
+        workExp:[]
       });
 
   },err=>{
@@ -43,9 +72,26 @@ export class ProfileComponent implements OnInit {
   })
   }
 
+
+  myClass(date:NgbDateStruct)
+{
+    let isSelected=this.especialDates
+         .find(d=>d.year==date.year && d.month==date.month && d.day==date.day)
+    return isSelected?'classSelected':'classNormal'
+}
+  isWeekend(date: NgbDateStruct) {
+    const d = new Date(date.year, date.month - 1, date.day);
+    return d.getDay() === 0 || d.getDay() === 6;
+  }
+
+  isDisabled(date: NgbDateStruct, current: {month: number}) {
+    return date.month !== current.month;
+  }
+
   onSubmit(){
     this.submitted = true;
     this.loading = true;
+    console.log("this.form.value",this.form.value)
     // this.accountService.register(this.form.value)
     // .pipe(first())
     // .subscribe(
